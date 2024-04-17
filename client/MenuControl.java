2d3de6f27adcb7584c1260f0e7d8870c3687faf8
Kathryn Kaelin <kkaelin@cub.uca.edu>
2024-04-17 11:51:36 -0500

    Controller constructors/ data fields
    Created constructors and added GameClient data fields for all the controllers

--- client/MenuControl.java
+++ client/MenuControl.java
@@ -1,5 +1,14 @@
 package client;
 
-public class MenuControl {
+import javax.swing.JPanel;
 
+public class MenuControl {
+	// Private data fields for the container and game client.
+	private JPanel container;
+	private GameClient client;
+	
+	public MenuControl(JPanel container, GameClient client) {
+		this.container = container;
+		this.client = client;
+	}
 }
