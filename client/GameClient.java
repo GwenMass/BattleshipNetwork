cb1936fe80109c0b56e7edbd58dc47e5e9121c67
Kathryn Kaelin <kkaelin@cub.uca.edu>
2024-04-16 19:18:53 -0500

    Started on client communication
    Added Error class and started on GameClient class

--- client/GameClient.java
+++ client/GameClient.java
@@ -1,5 +1,35 @@
 package client;
 
-public class GameClient {
+import ocsf.client.AbstractClient;
 
+public class GameClient extends AbstractClient{
+	//Data fields
+	
+	//Setters for controllers
+	
+	//Constructor
+	public GameClient() {
+		super("localhost", 8300);
+	}
+	
+	//Handles messages from server
+	public void handleMessageFromServer(Object arg0) {
+		
+		//If string was received, figure out event
+		if (arg0 instanceof String) {
+			
+			// Get the text of the message.
+		    String message = (String)arg0;
+		    
+		}
+		
+		else if (arg0 instanceof Error) {
+			
+			// Get the Error object.
+		    Error error = (Error)arg0;
+		    
+		}
+		
+		
+	}
 }
