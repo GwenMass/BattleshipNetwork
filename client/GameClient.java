50fb048566891042bab0014e90e43e406724382b
Kathryn Kaelin <kkaelin@cub.uca.edu>
2024-04-17 11:32:20 -0500

    Revert "Started on client communication"
    This reverts commit cb1936fe80109c0b56e7edbd58dc47e5e9121c67.

--- client/GameClient.java
+++ client/GameClient.java
@@ -1,35 +1,5 @@
 package client;
 
-import ocsf.client.AbstractClient;
+public class GameClient {
 
-public class GameClient extends AbstractClient{
-	//Data fields
-	
-	//Setters for controllers
-	
-	//Constructor
-	public GameClient() {
-		super("localhost", 8300);
-	}
-	
-	//Handles messages from server
-	public void handleMessageFromServer(Object arg0) {
-		
-		//If string was received, figure out event
-		if (arg0 instanceof String) {
-			
-			// Get the text of the message.
-		    String message = (String)arg0;
-		    
-		}
-		
-		else if (arg0 instanceof Error) {
-			
-			// Get the Error object.
-		    Error error = (Error)arg0;
-		    
-		}
-		
-		
-	}
 }
